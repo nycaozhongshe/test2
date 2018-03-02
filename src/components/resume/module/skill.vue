366a6abfea38d18cc6d4514e0021a272bbf5c37d
jiasheng <17710426384@163.com>
2018-03-02 17:03:27 +0800

    skill-style

--- src/components/resume/module/skill.vue
+++ src/components/resume/module/skill.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="skill-module">
     <div class="skill-title module-title">
-      3技能证书
+      技能证书
     </div>
+    <p>专业技能</p>
+    <p>语言技能</p>
+    <p>计算机技能</p>
+    <p>其它技能</p>
   </div>
 </template>
 
@@ -18,6 +22,7 @@
     border-bottom: 1px dashed #cfcfcf;
     .module-title {
       border-bottom: 1px solid #000000;
+      font-size: 26px;
     }
   }
 </style>
