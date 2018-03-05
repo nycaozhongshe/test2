ea056329b5a29a3f164d2751de9f971c71802a38
jiasheng <17710426384@163.com>
2018-03-05 08:59:41 +0800

    skill-style-1

--- src/components/resume/module/skill.vue
+++ src/components/resume/module/skill.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="skill-module">
     <div class="skill-title module-title">
-      技能证书<span @click="updateProfessionalSkills">修改</span>
+      技能证书
     </div>
-    <p>
-      专业技能
-      <span v-if="skillflag">{{pro_value || "专业技能"}}</span>
-    </p>
-    <p>语言技能 <span>语言技能。</span></p>
-    <p>计算机技能 <span>计算机技能。</span></p>
-    <p>其它技能 <span>其它技能。</span></p>
+    <table class="skill-tab">
+      <tr>
+        <th>专业技能</th>
+        <td><input type="text" class="input-skill" placeholder="专业技能"/></td>
+      </tr>
+      <tr>
+        <th>语言技能</th>
+        <td><input type="text" class="input-skill" placeholder="语言技能"/></td>
+      </tr>
+      <tr>
+        <th>计算机技能</th>
+        <td><input type="text" class="input-skill" placeholder="计算机技能"/></td>
+      </tr>
+      <tr>
+        <th>其它技能</th>
+        <td><input type="text" class="input-skill" placeholder="其它技能"/></td>
+      </tr>
+    </table>
   </div>
   <!--<span v-if="!skillflag"><input type="text" @blur="saveProfessionalSkills" v-model="pro_value"/></span>-->
 </template>
@@ -18,23 +29,19 @@
   export default {
     data() {
       return {
-        skillflag: true,
         pro_value: '',
+        lan_value: '',
+        com_value: '',
+        oth_value: '',
       }
     },
-    methods: {
-      updateProfessionalSkills() {
-        this.skillflag = false ;
-      },
-      saveProfessionalSkills() {
-        this.skillflag = true ;
-      }
-    }
+    methods: {}
   }
 </script>
 
 <style lang="scss" scoped>
   .skill-module {
+    width: 100%;
     height: auto;
     margin: 0 40px;
     padding: 30px 0 50px;
@@ -42,6 +49,58 @@
     .module-title {
       border-bottom: 1px solid #000000;
       font-size: 26px;
+      width: 90%;
+    }
+
+  }
+
+  .input-skill {
+    width: 100%;
+    padding-left: 10px;
+    background-color: #F0F0F0;
+    border: none;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    position: relative;
+    &:focus {
+      border-bottom: 1px dashed #666;
+      z-index: 999;
+    }
+    &::-webkit-input-placeholder {
+      color: #9e9e9e;
+    }
+    &:-moz-placeholder {
+      color: #9e9e9e;
+    }
+    &::-moz-placeholder {
+      color: #9e9e9e;
+    }
+    &:-ms-input-placeholder {
+      color: #9e9e9e;
     }
   }
+
+  .skill-tab {
+    width: 90%;
+    margin-top: 10px;
+    background-color: #F0F0F0;
+    tr {
+      height: 30px;
+      th {
+        width: 12%;
+        text-align: right;
+      }
+      td {
+        padding-left: 35px;
+      }
+    }
+    :hover {
+      background-color: #EBEBEB;
+      input {
+        background: #EBEBEB;
+      }
+    }
+  }
+
 </style>
