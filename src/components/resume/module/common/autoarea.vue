1893ac8f4d8afd64ba59b9fa6eb7928b4451b496
Rae <houxiaoyu1993@126.com>
2018-03-05 16:07:26 +0800

    b

---
+++ src/components/resume/module/common/autoarea.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="autoarea">
+      <div class="expandingArea">
+        <pre><span>{{text}}</span><br></pre>
+        <textarea type="text" v-model="text" class="module-input" cols="30"></textarea>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      props: [
+        'textContent'
+      ],
+      data() {
+        return {
+          text: this.textContent
+        }
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+  .expandingArea {
+    position: relative;
+    width: auto;
+    textarea{
+      position:absolute;
+      top:0;
+      left:0;
+      height:100%;
+      resize: none;
+      width: 100%;
+    }
+    pre{
+      display:block;
+      visibility:hidden;
+      span {
+        width: 100%;
+        display: inline-block;
+        height: auto;
+        white-space: normal;
+      }
+    }
+  }
+</style>
--- src/components/resume/module/education.vue
+++ src/components/resume/module/education.vue
@@ -1,68 +1,74 @@
 <template>
-    <div class="education-module">
-      <div class="education-title module-title">
-        教育背景
-        <div class="new-edu" @click="newEdu">
-          新增教育经历
-        </div>
+  <div class="education-module">
+    <div class="education-title module-title">
+      教育背景
+      <div class="new-edu" @click="newEdu">
+        新增教育经历
       </div>
-      <div class="education-list">
-        <ul>
-          <li v-for="(item, index) in eduList">
-            <div class="education-item clearfix">
-              <div class="education-item-context edu-time">
-                <el-date-picker
+    </div>
+    <div class="education-list">
+      <ul>
+        <li v-for="(item, index) in eduList">
+          <div class="education-item clearfix">
+            <div class="education-item-context edu-time">
+              <el-date-picker
                 type="daterange"
                 v-model="item.edu_time"
                 range-separator="至"
                 start-placeholder="开始日期"
                 end-placeholder="结束日期"
                 size="mini">
-              </el-date-picker></div>
-              <div class="education-item-context edu-school">
-                <input type="text" v-model="item.school_name" class="module-input">
-              </div>
-              <div class="education-item-context edu-pro">
-                <input type="text" v-model="item.major" class="module-input">
-              </div>
-              <div class="education-item-context edu-degree">
-                <input type="text" v-model="item.school_which" class="module-input">
-              </div>
+              </el-date-picker>
             </div>
-            <div class="education-other">
-              <div class="expandingArea">
-                <pre><span>{{item.edu_other}}</span><br></pre>
-                <textarea type="text" v-model="item.edu_other" class="module-input" cols="30"></textarea>
-              </div>
+            <div class="education-item-context edu-school">
+              <input type="text" v-model="item.school_name" class="module-input">
             </div>
-          </li>
-        </ul>
-      </div>
+            <div class="education-item-context edu-pro">
+              <input type="text" v-model="item.major" class="module-input">
+            </div>
+            <div class="education-item-context edu-degree">
+              <input type="text" v-model="item.school_which" class="module-input">
+            </div>
+          </div>
+          <div class="education-other">
+            <!--<div class="expandingArea">-->
+              <!--<pre><span>{{item.edu_other}}</span><br></pre>-->
+              <!--<textarea type="text" v-model="item.edu_other" class="module-input" cols="30"></textarea>-->
+            <!--</div>-->
+            <autoarea :text-content="item.edu_other"></autoarea>
+          </div>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
   import defaultData from '../js/app'
-    export default {
-      data() {
-        return {
-          eduList: [
-            {
-              edu_time: [],
-              school_name: '马歇尔商学院',
-              major: '金融学',
-              school_which: '硕士',
-              edu_other: '在这里详细描述你在校期间所学的专业，主要包括课程内容，校内任职，校内荣誉等内容...'
-            }
-          ],
-        }
-      },
-      methods: {
-        newEdu() {
-          this.eduList.push(defaultData['edu'])
-        }
+  import autoarea from './common/autoarea'
+  export default {
+    components: {
+      autoarea
+    },
+    data() {
+      return {
+        eduList: [
+          {
+            edu_time: [],
+            school_name: '马歇尔商学院',
+            major: '金融学',
+            school_which: '硕士',
+            edu_other: '在这里详细描述你在校期间所学的专业，主要包括课程内容，校内任职，校内荣誉等内容...'
+          }
+        ],
+      }
+    },
+    methods: {
+      newEdu() {
+        this.eduList.push(defaultData['edu'])
       }
     }
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -93,28 +99,28 @@
         }
       }
       .education-other {
-        .expandingArea {
-          position: relative;
-          width: auto;
-          textarea{
-            position:absolute;
-            top:0;
-            left:0;
-            height:100%;
-            resize: none;
-            width: 100%;
-          }
-          pre{
-            display:block;
-            visibility:hidden;
-            span {
-              width: 100%;
-              display: inline-block;
-              height: auto;
-              white-space: normal;
-            }
-          }
-        }
+        /*.expandingArea {*/
+        /*position: relative;*/
+        /*width: auto;*/
+        /*textarea{*/
+        /*position:absolute;*/
+        /*top:0;*/
+        /*left:0;*/
+        /*height:100%;*/
+        /*resize: none;*/
+        /*width: 100%;*/
+        /*}*/
+        /*pre{*/
+        /*display:block;*/
+        /*visibility:hidden;*/
+        /*span {*/
+        /*width: 100%;*/
+        /*display: inline-block;*/
+        /*height: auto;*/
+        /*white-space: normal;*/
+        /*}*/
+        /*}*/
+        /*}*/
       }
     }
   }
